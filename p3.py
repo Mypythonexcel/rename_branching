3667a2459c7cafb220af6184723e1a5478bff48d
Mypythonexcel <python2024devloper@gmail.com>
2025-01-25 18:10:13 +0530

    second commit on the second brach

--- p3.py
+++ p3.py
@@ -13,3 +13,12 @@ print("vvvvvvvvvvvvvvvvvvvvvv")
 print("ccccccccccccccccccccccccccc")
 print("ccccccccccccccccccccccccccccccccc")
 print("cxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+
+print("mujhe gussa aa raha ha ")
+print("kdjfslkdjflskd")
+print("baccho hum aur ek aur code push karege")
+
+print("qki jab app logo k sath unke jaise behave krte ho tho tab bhi unhe problem hoti ha ")
+
+print("dslkdfjlkdjflksdjflkdjflksdfjlkdjflkdsfjdlk")
