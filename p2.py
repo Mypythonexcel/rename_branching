973215204a2ee9e4dde1007f267388ead39c0756
Mypythonexcel <python2024devloper@gmail.com>
2025-01-25 16:15:08 +0530

    p2 pattern is added

---
+++ p2.py
@@ -0,0 +1,14 @@
+"""
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+
+"""
+
+for x in range(6):
+    for y in range(1,x+1):
+        print(y,end=" ")
+
+    print()
---
+++ pattern1.py
@@ -0,0 +1,21 @@
+
+for x in range(0,10):
+    for y in range(x+1):
+        print("x",end=" ")
+
+    print()
+     
+
+"""
+x  
+x x  
+x x x  
+x x x x  
+x x x x x  
+x x x x x x  
+x x x x x x x  
+x x x x x x x x  
+x x x x x x x x x  
+x x x x x x x x x x  
+
+"""
