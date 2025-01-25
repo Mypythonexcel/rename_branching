3b7fc08fcdd3b8511bff666483a60fc6bba3bdbb
Mypythonexcel <python2024devloper@gmail.com>
2025-01-25 17:51:56 +0530

    second commit on web page

--- p3.py
+++ p3.py
@@ -3,3 +3,7 @@ print("please push this code on main")
 print("ccccccccccccccccccccccccccccc")
 print("mmmmmmmmmmmmmmmmmmmmmmmm")
 print("nnnnnnnnnnnnnnnnnnnnnnnnnnn")
+
+
+print("i am very angry")
+print("i am really very angry !!!!!!!!!!!!!!!!")
